b5fdb9c6d02176f07963b07034d0e24a1a74cbed
namnv1 <namnv1@vmodev.com>
2022-07-16 18:42:49 +0700

    update be

--- backend/src/api/students/students.dto.ts
+++ backend/src/api/students/students.dto.ts
@@ -1,103 +1,69 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
 
 export class StudentRegisterdto {
   @ApiProperty()
-  @IsNotEmpty()
   classId: number;
 
   @ApiProperty({ example: 'Iris123' })
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(40)
   username: string;
 
   @ApiProperty({ example: 'Nguyen Van Nam' })
-  @IsNotEmpty()
-  @IsString()
-  @MaxLength(40)
   name: string;
 
   @ApiProperty({ example: 'iris123@gmail.com' })
-  @IsNotEmpty()
-  @IsEmail()
   email: string;
 
   @ApiProperty({ example: 'Nam123' })
-  @MinLength(6)
-  @IsNotEmpty()
   password: string;
 
   @ApiProperty({ example: 'Nam123' })
-  @IsNotEmpty()
   sex: string;
 
   @ApiProperty({ example: '2021-02-19' })
-  @IsNotEmpty()
   dateOfBirth: Date;
 
   @ApiProperty({ example: '2021-02-19' })
-  @IsNotEmpty()
   address: string;
 
   @ApiProperty({ example: 243242473 })
-  @IsNotEmpty()
   phone: number;
 
   @ApiProperty({ example: '2021-02-19' })
-  @IsNotEmpty()
   startStudying: Date;
 
   @ApiProperty({ example: '20rwtr9' })
-  @IsNotEmpty()
   fatherName: string;
 
   @ApiProperty({ example: '20rwtr9' })
-  @IsNotEmpty()
   fatherJob: string;
 
   @ApiProperty({ example: 4324214 })
-  @IsNotEmpty()
   fatherPhone: number;
 
   @ApiProperty({ example: '1943-09-03' })
-  @IsNotEmpty()
   fatherDateOfBirth: Date;
 
   @ApiProperty({ example: '31 ngo Duy Hung, tp Ha Noi' })
-  @IsNotEmpty()
   fatherJobAddress: string;
 
   @ApiProperty({ example: 'Noi' })
-  @IsNotEmpty()
   motherName: string;
 
   @ApiProperty({ example: 'fsadfsdfi' })
-  @IsNotEmpty()
   motherJob: string;
 
   @ApiProperty({ example: 243242473 })
-  @IsNotEmpty()
   motherPhone: number;
 
   @ApiProperty({ example: '1943-09-03' })
-  @IsNotEmpty()
   motherDateOfBirth: Date;
 
   @ApiProperty({ example: '31 ngo Duy Hung, tp Ha Noi' })
-  @IsNotEmpty()
   motherJobAddress: string;
 }
 
 export class GetOneStudentdto {
   @ApiProperty()
-  @IsNotEmpty()
   id: number;
 }
 
@@ -179,6 +145,5 @@ export class SearchStudentdto {
 
 export class DeleteStudentdto {
   @ApiProperty()
-  @IsNotEmpty()
   id: number;
 }
